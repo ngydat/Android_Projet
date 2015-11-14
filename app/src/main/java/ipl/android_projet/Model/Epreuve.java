c3cd02b6388201ab7fe15fe1f1a363bf7ae30110
Giordano <gio.agnello@gmail.com>
2015-11-14 16:06:32 +0100

    Ajout Epreuve.java dans Model

---
+++ app/src/main/java/ipl/android_projet/Model/Epreuve.java
@@ -0,0 +1,55 @@
+package ipl.android_projet.model;
+
+import java.net.URI;
+
+/**
+ * Created by Giordano on 14/11/2015.
+ */
+public class Epreuve {
+
+
+    private int point;
+    private String type;
+    private URI uri;
+    private boolean termine;
+
+    public Epreuve(int point, String type,URI uri,boolean termine){
+        this.point = point;
+        this.type = type;
+        this.uri = uri;
+        this.termine=termine;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public boolean isTermine() {
+        return termine;
+    }
+
+    public void setTermine(boolean termine) {
+        this.termine = termine;
+    }
+}
